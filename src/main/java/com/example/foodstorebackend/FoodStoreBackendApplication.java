d042fdc03ce01ceab63e28c1a46bd4156000c892
JAWAD <76213681+mdjawad0@users.noreply.github.com>
2022-10-06 13:52:28 +0530

    Update FoodStoreBackendApplication.java

--- src/main/java/com/example/foodstorebackend/FoodStoreBackendApplication.java
+++ src/main/java/com/example/foodstorebackend/FoodStoreBackendApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class FoodStoreBackendApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.location", "application.properties");
 		SpringApplication.run(FoodStoreBackendApplication.class, args);
 	}
 
